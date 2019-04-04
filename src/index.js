14f021ee7f1bd54163748ec8da5c5c841f3d571d
Lais Frigério <laisfrigerio@hotmail.com>
2019-04-04 07:18:25 -0300

    Add CSS no component App
    Signed-off-by: Lais Frigério <laisfrigerio@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class App extends React.Component {
-    render() {
-        return <button>Entrar</button>
-    }
-}
+import App from './components/App';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
