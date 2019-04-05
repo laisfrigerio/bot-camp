6dda882eda9ddf9a08b9548b7aa34c0bb0c46f60
Lais Frigério <laisfrigerio@hotmail.com>
2019-04-05 07:22:31 -0300

    Add o component LoginWrapper
    Signed-off-by: Lais Frigério <laisfrigerio@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import BtnBotcamp from './components/BtnBotcamp/index';
 import LogoBotcamp from './components/LogoBotcamp/index';
+import LoginWrapper from './components/LoginWrapper/index';
 import './styles/reset.css';
 
 class App extends Component {
     render() {
         return (
-            <Fragment>
+            <LoginWrapper>
                 <LogoBotcamp/>
                 <BtnBotcamp/>
-            </Fragment>
+            </LoginWrapper>
         );
     }
 }
---
+++ src/components/LoginWrapper/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import background from '../../img/background.svg';
+
+const LoginWrapper = styled.main`
+    width: 100vw;
+    height: 100vh;
+    background-image: url(${background});
+    background-repeat: no-repeat;
+`;
+
+export default LoginWrapper;
