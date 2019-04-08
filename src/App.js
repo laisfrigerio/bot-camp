3090dedaf1a0e063ef41098e0b95404bf674c8f6
Lais Frigério <laisfrigerio@hotmail.com>
2019-04-08 07:08:00 -0300

    Add react router
    Signed-off-by: Lais Frigério <laisfrigerio@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import './styles/reset.css';
-// import Login from "./pages/Login";
+import Login from "./pages/Login";
 import Chat from "./pages/Chat";
+import './styles/reset.css';
 
 class App extends Component {
     render() {
         return (
-            <Chat />
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/' exact component={Login}/>
+                    <Route path='/chat' exact component={Chat}/>
+                </Switch>
+            </BrowserRouter>
         );
     }
 }
